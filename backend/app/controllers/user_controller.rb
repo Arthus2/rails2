e672b6c650def8d19803e40b2c4bb46e4870c81c
Aude Cervantes <aude.cervantes@hotmail.fr>
2018-04-26 17:28:47 +0200

    redirect to

---
+++ backend/app/controllers/user_controller.rb
@@ -0,0 +1,12 @@
+class UserController < ApplicationController
+    def new
+    end
+    def create
+        user = User.new
+        user.username = params["username"]
+        user.bio = params["bio"]
+        user.save
+        puts user
+        redirect_to "http://localhost:3000/"
+    end
+end
